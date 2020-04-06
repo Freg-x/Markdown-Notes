6cc356052551d54aa9e42bd0335d88ee0bd0f65a
Freg-x <1093893470@qq.com>
2020-04-06 20:41:11 +0800

    2020/04/06

---
+++ Design_Pattern/dp/dp/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  dp
+//
+//  Created by Freg Wang on 2020/4/6.
+//  Copyright © 2020 Freg Wang. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
